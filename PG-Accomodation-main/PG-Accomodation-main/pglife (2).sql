ce58363f2103d2c944f7462f701d4835059c46de
Disha G K <119435280+dishagk@users.noreply.github.com>
2024-04-17 22:43:02 +0530

    Add files via upload

---
+++ PG-Accomodation-main/PG-Accomodation-main/pglife (2).sql
@@ -0,0 +1,427 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 27, 2023 at 08:09 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pglife`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `intr_user` ()   SELECT  *   FROM interested_users_properties iup
+ INNER JOIN properties p ON iup.property_id = p.id
+ WHERE iup.user_id = $user_id$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `amenities`
+--
+
+CREATE TABLE `amenities` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `icon` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `amenities`
+--
+
+INSERT INTO `amenities` (`id`, `name`, `type`, `icon`) VALUES
+(1, 'Wifi', 'Common Area', 'wifi'),
+(2, 'Power Backup', 'Building', 'powerbackup'),
+(3, 'Fire Extinguisher', 'Building', 'fireext'),
+(4, 'TV', 'Common Area', 'tv'),
+(5, 'Bed with Mattress', 'Bedroom', 'bed'),
+(6, 'Parking', 'Building', 'parking'),
+(7, 'Water Purifier', 'Common Area', 'rowater'),
+(8, 'Dining', 'Common Area', 'dining'),
+(9, 'Air Conditioner', 'Bedroom', 'ac'),
+(10, 'Washing Machine', 'Common Area', 'washingmachine'),
+(11, 'Lift', 'Building', 'lift'),
+(12, 'CCTV', 'Building', 'cctv'),
+(13, 'Geyser', 'Washroom', 'geyser');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cities`
+--
+
+INSERT INTO `cities` (`id`, `name`) VALUES
+(1, 'Sullia'),
+(2, 'Puttur'),
+(3, 'Mangaluru'),
+(4, 'Bengaluru');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `interested_users_properties`
+--
+
+CREATE TABLE `interested_users_properties` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `property_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `interested_users_properties`
+--
+
+INSERT INTO `interested_users_properties` (`id`, `user_id`, `property_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 5),
+(4, 2, 1),
+(5, 2, 5),
+(6, 3, 1),
+(7, 3, 2),
+(8, 3, 5),
+(9, 4, 2),
+(10, 4, 3),
+(11, 4, 4),
+(16, 5, 1),
+(17, 5, 11),
+(18, 5, 2),
+(19, 5, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `properties`
+--
+
+CREATE TABLE `properties` (
+  `id` int(11) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `description` longtext NOT NULL,
+  `gender` enum('male','female','unisex') NOT NULL,
+  `rent` int(11) NOT NULL,
+  `rating_clean` float(2,1) NOT NULL,
+  `rating_food` float(2,1) NOT NULL,
+  `rating_safety` float(2,1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `properties`
+--
+
+INSERT INTO `properties` (`id`, `city_id`, `name`, `address`, `description`, `gender`, `rent`, `rating_clean`, `rating_food`, `rating_safety`) VALUES
+(1, 1, 'Sannidhi\\\'s Paying Guest', 'H.No. 3958 near town hall,Kerpala Road,Amara Sullia,Sullia-DK 574239', 'Furnished studio apartment - share it with close friends! Located in Sullia , this house is available for both boys and girls. Go for a private room or opt for a shared one and make it your own abode. Go out with your new friends - catch a movie at the nearest cinema hall or just chill in a cafe which is not even 2 kms away. Unwind with your flatmates after a long day at work/college. With a common living area and a shared kitchen, make your own FRIENDS moments. After all, there\\\'s always a Joey with unlimited supply of food. Remember, all it needs is one crazy story to convert a roomie into a BFF.You will get a every peace environmnent.', 'male', 5000, 4.3, 3.4, 4.8),
+(2, 1, 'Navarathna PG Home', '644-C,NEAR PRIVATE BUSSTAND,Shivakripa Hall,Sullia-DK 574239', 'This PG is Loacated nearer to most of the Education Centers.There is a Movie theater with in 1km,you can send ur sunday or weekend with your friends. And also there is hotels nearer to the PGs.Unwind with your flatmates after a long day at work/college.there is common living area and 4 beds in each room and also a study.There is a hotels nearer to the PG\\s.No food facilities you can use the near by canteens or hotels for food.There is a peace environment.You can have a every good and joyfull life here.', 'unisex', 6000, 2.9, 3.4, 3.8),
+(3, 2, 'Samrudhi Paying Guest', '44, near Neharu-Nagar,Puttur-DK', 'Furnished studio apartment - share it with close friends! Located in posh area of Puttur, this house is available for both boys and girls. Go for a private room or opt for a shared one and make it your own abode. Go out with your new friends - catch a movie at the nearest cinema hall or just chill in a cafe which is not even 2 kms away. Unwind with your flatmates after a long day at work/college. With a common living area and a shared kitchen, make your own FRIENDS moments. After all, there\\\'s always a Joey with unlimited supply of food. Remember, all it needs is one crazy story to convert a roomie into a BFF. What\\\'s nearby/Your New Neighborhood 4.0 Kms from Dwarka Sector- 21 Metro Station.', 'female', 9500, 3.9, 3.8, 4.9),
+(4, 2, 'PG for Girls Sharada', 'Plot no.258/D4, near philomena church,darbe Puttur-574201', 'Furnished studio apartment-Share it with a close friends!.This PG is located near the phylomina college Puttur. There is a mall within 1km,where you can spend your weekend with your friends.Here there is many and many facilities with belongs to your studies which you can make benificiary of this.. With a common living area and a shared kitchen, make your own FRIENDS moments. After all, there\\\'s always a Joy with unlimited supply of food. Remember, all it needs is one crazy story to convert a roomie into a BFF.There is a canteen within 1km.And also you will get a tasty food in this PGs.The environemnt is peacefull and is very much adjustable for the students', 'female', 8000, 4.2, 4.1, 4.5),
+(5, 2, 'Ganpati Paying Guest', 'Nearer to the Police station , Puttur-DK 574201', 'Furnished studio apartment - share it with close friends! Located in local area of Puttur , This PG is available only for the PU and degree students. Where there is a every good study environment.There is a very good food facilities where a students donot want to worry about their food. There are many schools and collegs nearer to the this PG.You will have a joy with your friends and cherrish a very good moments with friends.In weekends you can spend a time in park.', 'male', 8500, 4.2, 3.9, 4.6),
+(6, 3, 'Govindh\\\'s boys PG', 'H.No. 35 near to the shradda textile, Jyothi , Mangaluru-DK 574241', 'Furnished studio apartment - share it with close friends! Located in Mangluru , this house is available for both boys and girls. Go for a private room or opt for a shared one and make it your own abode. Go out with your new friends - catch a movie at the nearest cinema hall or just chill in a cafe which is not even 2 kms away. Unwind with your flatmates after a long day at work/college. With a common living area and a shared kitchen, make your own FRIENDS moments. After all, there\\\'s always a Joey with unlimited supply of food. Remember, all it needs is one crazy story to convert a roomie into a BFF.You will get a every peace environmnent.', 'male', 6000, 4.5, 4.3, 4.0),
+(7, 3, 'Maithri paying Guest', '567-C,Near to the st.joseph engineering college , Mangaluru , 574241', 'This PG is Loacated nearer to most of the Education Centers.There is a Movie theater with in 1km,you can send ur sunday or weekend with your friends. And also there is hotels nearer to the PGs.Unwind with your flatmates after a long day at work/college.there is common living area and 4 beds in each room and also a study.There is a hotels nearer to the PG\\s.No food facilities you can use the near by canteens or hotels for food.There is a peace environment.You can have a every good and joyfull life here.', 'female', 6000, 2.9, 3.4, 3.8),
+(8, 3, 'Krishna PG for girls', 'H.No-066 , Opposite to the airport Road , Mangaluru-DK , 574241.', 'Furnished studio apartment - share it with close friends! Located in posh area of Mangluru, this house is available for both boys and girls. Go for a private room or opt for a shared one and make it your own abode. Go out with your new friends - catch a movie at the nearest cinema hall or just chill in a cafe which is not even 2 kms away. Unwind with your flatmates after a long day at work/college. With a common living area and a shared kitchen, make your own FRIENDS moments. After all, there\\\'s always a Joey with unlimited supply of food. Remember, all it needs is one crazy story to convert a roomie into a BFF.', 'female', 7500, 4.2, 3.5, 4.5),
+(9, 4, 'Kaashi Pg For Students', 'H.NO-77 , 3rd cross Near to Majestics , Bangalore', 'Furnished studio apartment - share it with close friends! Located in banglore, this house is available for both boys and girls. Go for a private room or opt for a shared one and make it your own abode. Go out with your new friends - catch a movie at the nearest cinema hall or just chill in a cafe which is not even 2 kms away. Unwind with your flatmates after a long day at work/college. With a common living area and a shared kitchen, make your own FRIENDS moments. After all, there\\\'s always a Joey with unlimited supply of food. Remember, all it needs is one crazy story to convert a roomie into a BFF.You will get a every peace environmnent.', 'female', 8300, 3.7, 4.2, 4.1),
+(10, 1, 'Kaveri paying guest', 'Near Vivekananda circle , JC road , Sullia Dk', 'This PG is Loacated nearer to most of the Education Centers.There is a Movie theater with in 1km,you can send ur sunday or weekend with your friends. And also there is hotels nearer to the PGs.Unwind with your flatmates after a long day at work/college.there is common living area and 4 beds in each room and also a study.There is a hotels nearer to the PG\\s.No food facilities you can use the near by canteens or hotels for food.There is a peace environment.You can have a every good and joyfull life here.', 'female', 7500, 3.7, 4.3, 4.0),
+(11, 1, 'Rajadani PG ', 'Near to the court road Sullia DK. ', 'This PG is Loacated nearer to most of the Education Centers.There is a Movie theater with in 1km,you can send ur sunday or weekend with your friends. And also there is hotels nearer to the PGs.Unwind with your flatmates after a long day at work/college.there is common living area and 4 beds in each room and also a study.There is a hotels nearer to the PG\\s.No food facilities you can use the near by canteens or hotels for food.There is a peace environment.You can have a every good and joyfull life here.', 'male', 9500, 3.7, 4.3, 4.5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `properties_amenities`
+--
+
+CREATE TABLE `properties_amenities` (
+  `id` int(11) NOT NULL,
+  `property_id` int(11) NOT NULL,
+  `amenity_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `properties_amenities`
+--
+
+INSERT INTO `properties_amenities` (`id`, `property_id`, `amenity_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 4),
+(4, 1, 5),
+(5, 1, 7),
+(6, 1, 8),
+(7, 1, 9),
+(8, 1, 10),
+(9, 1, 11),
+(10, 1, 13),
+(11, 2, 1),
+(12, 2, 2),
+(13, 2, 3),
+(14, 2, 4),
+(15, 2, 5),
+(16, 2, 7),
+(17, 2, 8),
+(18, 2, 9),
+(19, 2, 10),
+(20, 2, 11),
+(21, 2, 13),
+(22, 3, 1),
+(23, 3, 2),
+(24, 3, 3),
+(25, 3, 4),
+(26, 3, 5),
+(27, 3, 7),
+(28, 3, 8),
+(29, 3, 10),
+(30, 3, 11),
+(31, 3, 12),
+(32, 3, 13),
+(33, 4, 1),
+(34, 4, 3),
+(35, 4, 4),
+(36, 4, 5),
+(37, 4, 7),
+(38, 4, 8),
+(39, 4, 10),
+(40, 4, 11),
+(41, 4, 12),
+(42, 4, 13),
+(43, 5, 1),
+(44, 5, 3),
+(45, 5, 4),
+(46, 5, 5),
+(47, 5, 7),
+(48, 5, 8),
+(49, 5, 10),
+(50, 5, 11),
+(51, 5, 12),
+(52, 5, 13);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testimonials`
+--
+
+CREATE TABLE `testimonials` (
+  `id` int(11) NOT NULL,
+  `property_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `content` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `testimonials`
+--
+
+INSERT INTO `testimonials` (`id`, `property_id`, `user_name`, `content`) VALUES
+(1, 1, 'Santhosh Kumar', 'You just have to arrive at the place, it\'s fully furnished and stocked with all basic amenities and services and even your friends are welcome.'),
+(2, 1, 'Karna', 'You just have to arrive at the place, it\'s fully furnished and stocked with all basic amenities and services and even your friends are welcome.'),
+(3, 2, 'Sandesh', 'You just have to arrive at the place, it\'s fully furnished and stocked with all basic amenities and services and even your friends are welcome.'),
+(4, 2, 'Rajkumar', 'You just have to arrive at the place, it\'s fully furnished and stocked with all basic amenities and services and even your friends are welcome.'),
+(5, 2, 'Sudeep', 'You just have to arrive at the place, it\'s fully furnished and stocked with all basic amenities and services and even your friends are welcome.'),
+(6, 3, 'Rajesh', 'You just have to arrive at the place, it\'s fully furnished and stocked with all basic amenities and services and even your friends are welcome.'),
+(7, 3, 'Prajwal', 'You just have to arrive at the place, it\'s fully furnished and stocked with all basic amenities and services and even your friends are welcome.'),
+(8, 4, 'Pratheek', 'You just have to arrive at the place, it\'s fully furnished and stocked with all basic amenities and services and even your friends are welcome.'),
+(9, 5, 'Prasanna\r\n', 'You just have to arrive at the place, it\'s fully furnished and stocked with all basic amenities and services and even your friends are welcome.'),
+(10, 5, 'Amruth Raj\r\n', 'You just have to arrive at the place, it\'s fully furnished and stocked with all basic amenities and services and even your friends are welcome.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `gender` enum('male','female') NOT NULL,
+  `college_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `full_name`, `phone`, `gender`, `college_name`) VALUES
+(1, 'anuj.kalbalia@gmail.com', 'b1b3773a05c0ed0176787a4f1574ff0075f7521e', 'Anuj Kalbalia', '9535100112', 'male', 'NITD'),
+(2, 'shadab@gmail.com', 'b1b3773a05c0ed0176787a4f1574ff0075f7521e', 'Shadab Alam', '9876543210', 'male', 'NITJ'),
+(3, 'aditya@gmail.com', 'b1b3773a05c0ed0176787a4f1574ff0075f7521e', 'Aditya Sood', '9876543210', 'male', 'Chandigarh University'),
+(4, 'radhika@gmail.com', 'b1b3773a05c0ed0176787a4f1574ff0075f7521e', 'Radhika Bhatia', '9876543210', 'female', 'Delhi University'),
+(5, 'sapthamichemnoor029@gmail.com', '515c1311fcb803ec44a7ae989bd404e3a6643276', 'sapthami CG', '8792019339', 'female', 'kvg college of engineering sullia'),
+(19, 'nanmakrishnan@gmail.com', '2d495fbcffe5a479b33b05446afd305d6fef3fa9', 'nanma krishanan', '8746862155', 'female', 'kvg college of engineering sullia');
+
+--
+-- Triggers `users`
+--
+DELIMITER $$
+CREATE TRIGGER `phone_trigger` AFTER INSERT ON `users` FOR EACH ROW BEGIN
+         if length(new.phone)>10 or length(new.phone)<10 
+             THEN 
+             SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'phone number is invalid';
+                 END IF; 
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `amenities`
+--
+ALTER TABLE `amenities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `interested_users_properties`
+--
+ALTER TABLE `interested_users_properties`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `property_id` (`property_id`);
+
+--
+-- Indexes for table `properties`
+--
+ALTER TABLE `properties`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `city_id` (`city_id`);
+
+--
+-- Indexes for table `properties_amenities`
+--
+ALTER TABLE `properties_amenities`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `property_id` (`property_id`),
+  ADD KEY `amenity_id` (`amenity_id`);
+
+--
+-- Indexes for table `testimonials`
+--
+ALTER TABLE `testimonials`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `property_id` (`property_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `amenities`
+--
+ALTER TABLE `amenities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `interested_users_properties`
+--
+ALTER TABLE `interested_users_properties`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `properties`
+--
+ALTER TABLE `properties`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `properties_amenities`
+--
+ALTER TABLE `properties_amenities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `testimonials`
+--
+ALTER TABLE `testimonials`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `interested_users_properties`
+--
+ALTER TABLE `interested_users_properties`
+  ADD CONSTRAINT `interested_users_properties_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `interested_users_properties_ibfk_2` FOREIGN KEY (`property_id`) REFERENCES `properties` (`id`);
+
+--
+-- Constraints for table `properties`
+--
+ALTER TABLE `properties`
+  ADD CONSTRAINT `properties_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`);
+
+--
+-- Constraints for table `properties_amenities`
+--
+ALTER TABLE `properties_amenities`
+  ADD CONSTRAINT `properties_amenities_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `properties` (`id`),
+  ADD CONSTRAINT `properties_amenities_ibfk_2` FOREIGN KEY (`amenity_id`) REFERENCES `amenities` (`id`);
+
+--
+-- Constraints for table `testimonials`
+--
+ALTER TABLE `testimonials`
+  ADD CONSTRAINT `testimonials_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `properties` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
